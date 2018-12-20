f46dc1fecbcd599997c7969e9242c74e4aa02084
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2018-12-20 19:38:44 +0330

    2. Set App.js - Attached InsuranceDB - Attached Another Files

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var sqlite = require('sqlite3');
+var env = require('dotenv').load();
+var port = process.env.PORT || 8000;
+
+var models = require("./models");
+var client = require('./routes/client');
+
+
+models.sequelize.sync().then(function() {
+    console.log('connected to database')
+}).catch(function(err) {
+    console.log(err)
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.use('/client', client);
+
+
+app.get('/', function(req, res){
+    console.log('app listening on port: '+port);
+    res.send('tes express nodejs sqlite')
+});
+
+app.listen(port, function(){
+    console.log('app listening on port: '+port);
+});
+
+module.exports = app;
